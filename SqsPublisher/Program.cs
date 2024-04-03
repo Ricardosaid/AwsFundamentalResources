16bb37f937a287ec8365c5d3fdfd9182d45233d0
Ricardo Martinez <ricardo@smartpaymx.com>
2024-04-02 21:54:21 -0600

    adding Sqs publisher

--- SqsPublisher/Program.cs
+++ SqsPublisher/Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text.Json;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using SqsPublisher;
 
-Console.WriteLine("Hello, World!");
+var sqsClient = new AmazonSQSClient();
+
+var customer = new CustomerCreated
+{
+    Id = Guid.NewGuid(),
+    Name = "John",
+    Email = "example@example.com",
+    FullName = "John Doe",
+    DateOfBirth = new DateTime(1993, 1, 1),
+    GithubUserName = "johndoe",
+};
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("Customers");
+
+//sending the message
+
+var sendMessageRequest = new SendMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(customer)
+};
+
+var response = await sqsClient.SendMessageAsync(sendMessageRequest);
+
+Console.WriteLine();
+
+
