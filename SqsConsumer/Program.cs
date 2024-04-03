e1b49a100610ff33cbf1ff4f84e0d27737e682e7
Ricardo Martinez <ricardo@smartpaymx.com>
2024-04-02 21:54:39 -0600

    adding sqs consumer

---
+++ SqsConsumer/Program.cs
@@ -0,0 +1,27 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+
+var cts = new CancellationTokenSource();
+var sqsClient = new AmazonSQSClient();
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("Customers");
+
+var receiveMessageRequest = new ReceiveMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl
+};
+
+while (!cts.IsCancellationRequested)
+{
+    var response = await sqsClient.ReceiveMessageAsync(receiveMessageRequest,cts.Token);
+
+    foreach (var message in response.Messages)
+    {
+        Console.WriteLine($"MessageId: {message.MessageId}");
+        Console.WriteLine($"ReceiptHandle: {message.ReceiptHandle}");
+        Console.WriteLine($"Body: {message.Body}");
+        await sqsClient.DeleteMessageAsync(queueUrlResponse.QueueUrl, message.ReceiptHandle, cts.Token);
+    }
+    await Task.Delay(3000);
+    
+}
