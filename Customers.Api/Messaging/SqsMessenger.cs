580bb9e8fefd2b40445e63120a26535f46ec6f85
Ricardo Martinez <ricardo@smartpaymx.com>
2024-04-04 23:53:26 -0600

    Adding sqsMessenger

---
+++ Customers.Api/Messaging/SqsMessenger.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.Json;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using Microsoft.Extensions.Options;
+
+namespace Customers.Api.Messaging;
+
+public class SqsMessenger : ISqsMessenger
+{
+    private readonly IAmazonSQS _sqs;
+    //Inject options
+    private readonly IOptions<QueueSettings> _queueSettings;
+    // Creating the queueUrl a singleton
+    private string? _queueUrl;
+
+    public SqsMessenger(IAmazonSQS sqs, IOptions<QueueSettings> queueSettings)
+    {
+        _sqs = sqs;
+        _queueSettings = queueSettings;
+    }
+
+    public async Task<SendMessageResponse> SendMessageAsync<T>(T message)
+    {
+        //1) Get the queue url
+        var queueUrl = await GetQueueUrlAsync();
+        //2) Create the send message request
+        var sendMessageRequest = new SendMessageRequest
+        {
+            QueueUrl = queueUrl,
+            MessageBody = JsonSerializer.Serialize(message),
+            MessageAttributes = new Dictionary<string, MessageAttributeValue>
+            {
+                {
+                    "MessageType", new MessageAttributeValue
+                    {
+                        DataType = "String",
+                        StringValue = typeof(T).Name
+                    }
+                }
+            }
+        };
+        //3) Send the message
+        return await _sqs.SendMessageAsync(sendMessageRequest);
+    }
+
+    private async Task<string> GetQueueUrlAsync()
+    {
+        if (_queueUrl is not null)
+        {
+            return _queueUrl;
+        }
+        var queueUrlResponse = await _sqs.GetQueueUrlAsync(_queueSettings.Value.Name);
+        _queueUrl = queueUrlResponse.QueueUrl;
+        return _queueUrl;
+    }
+}
